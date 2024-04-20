914d26e50197fdf123bfd05f64cd6484eb241b74
“Suzanna” <americandreamdoor@gmail.com>
2024-04-20 15:36:13 -0700

    Adding new algo to find the increasing triple sequence from arr. Add inset Interval. Add Interval.java.

---
+++ src/main/java/com/bank/leetcode/array/Interval.java
@@ -0,0 +1,9 @@
+package com.bank.leetcode.array;
+
+public class Interval {
+    int start;
+    int end;
+  Interval() { start = 0; end = 0; }
+  Interval(int s, int e) { start = s; end = e; }
+  }
+
